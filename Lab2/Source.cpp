26c50697f7fac529f39f7ac5a07e82996291ae56
Nicolas Perez-merino <nperezme@mail.umw.edu>
2025-05-21 17:52:25 -0400

    Movement keys will now update the circle's x and y position. Artifacts from previous circle positions are removed before a new circle is drawn.

--- Lab2/Source.cpp
+++ Lab2/Source.cpp
@@ -2,7 +2,7 @@
 #include <allegro5/allegro_primitives.h>
 #include <iostream>
 
-void handle_movement_keys(ALLEGRO_EVENT event);
+void handle_movement_keys(ALLEGRO_EVENT event, float& x, float& y);
 
 int main() {
 
@@ -18,6 +18,7 @@ int main() {
 	}
 	al_init_primitives_addon();
 
+	// Initliaze the event queue to handle keyboard events
 	ALLEGRO_EVENT_QUEUE* event_queue = NULL;
 	ALLEGRO_EVENT event;
 	event_queue = al_create_event_queue();
@@ -31,36 +32,49 @@ int main() {
 		std::cerr << "Keyboard not found!" << std::endl;
 		return -1;
 	}
-	// Allow keyboard events to be processed
+	// Allow keyboard interaction into event queue
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
 
+	// Initial circle position
+	float circle_x = 400;
+	float circle_y = 300;
+
 	while (true) {
+		// Clear the display with a black color (gets rid of the previous circle)
 		al_clear_to_color(al_map_rgb(0, 0, 0));
-		al_draw_circle(400, 300, 50, al_map_rgb(255, 50, 50), 5);
+		// Draw the circle at the new position
+		al_draw_circle(circle_x, circle_y, 25, al_map_rgb(255, 50, 50), 5);
+		// Swap buffers and display the new circle
+		al_flip_display();
 		// The listener for the event queue
 		al_wait_for_event(event_queue, &event);
 		// Check if the event is a movement key
-		handle_movement_keys(event);
-		al_flip_display();
+		handle_movement_keys(event, circle_x, circle_y);	
 	}
 
 	return 0;
 }
 
-void handle_movement_keys(ALLEGRO_EVENT event) {
+/*
+* Function to handle all valid movement keys to update the circle position,
+* both x and y coordinates. Only gets the down press of a key.
+* All conditional logic for key events are handled here to make game loop cleaner.
+*/
+void handle_movement_keys(ALLEGRO_EVENT event, float &x, float &y) {
+	float move_amount = 5.0f;
 	if (event.type == ALLEGRO_EVENT_KEY_DOWN) {
 		switch (event.keyboard.keycode) {
 			case ALLEGRO_KEY_UP:
-				std::cout << "Up key pressed!" << std::endl;
+				y -= move_amount;
 				break;
 			case ALLEGRO_KEY_DOWN:
-				std::cout << "Down key pressed!" << std::endl;
+				y += move_amount;
 				break;
 			case ALLEGRO_KEY_LEFT:
-				std::cout << "Left key pressed!" << std::endl;
+				x -= move_amount;
 				break;
 			case ALLEGRO_KEY_RIGHT:
-				std::cout << "Right key pressed!" << std::endl;
+				x += move_amount;
 				break;
 		}
 	}
